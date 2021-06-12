bf9c90ada985cb4d00f26ef8ce65af084075a0c3
lhs7091 <lhs7091@naver.com>
2021-06-12 14:37:32 +0900

    3-20 franc(duplicate several funtion)

--- tdd-by-example/src/test/java/guru/springframework/dollor/DollarTest.java
+++ tdd-by-example/src/test/java/guru/springframework/dollor/DollarTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DollarTest {
 
     @Test
-    void testMultiplication(){
+    void testMultiplicationDollar(){
         Dollar five = new Dollar(5);
 
         Dollar product = five.times(2);
@@ -18,8 +18,25 @@ class DollarTest {
     }
 
     @Test
-    void testEquality(){
+    void testEqualityDollar(){
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(8));
+    }
+
+    @Test
+    void testMultiplicationFranc(){
+        Franc five = new Franc(5);
+
+        Franc product = five.times(2);
+        assertEquals(new Franc(10), product);
+
+        product = five.times(3);
+        assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    void testEqualityFranc(){
+        assertEquals(new Franc(5), new Franc(5));
+        assertNotEquals(new Franc(5), new Franc(8));
     }
 }
