ee795a904bf38c8479c047f370f9e7d87f274f3c
lhs7091 <lhs7091@naver.com>
2021-06-12 10:58:18 +0900

    3-16 Multi-Currency Money

---
+++ tdd-by-example/src/main/java/guru/springframework/dollor/Dollar.java
@@ -0,0 +1,14 @@
+package guru.springframework.dollor;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount){
+        this.amount = amount;
+    }
+
+    void times(int multiplier){
+        amount *= multiplier;
+    }
+}
