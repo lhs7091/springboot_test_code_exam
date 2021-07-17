8457fed619c87e2588b4748753207f36686f3473
lhs7091 <lhs7091@naver.com>
2021-07-17 21:31:28 +0900

    10-118 BDD Verification in Mockito

--- testing-junit5-mockito/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ testing-junit5-mockito/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -53,15 +54,20 @@ class SpecialitySDJpaServiceTest {
 
     @Test
     void findByIdBddTest(){
+        // given
         Speciality speciality = new Speciality();
-
         given(specialtyRepository.findById(1L)).willReturn(Optional.of(speciality));
 
+        // when
         Speciality findSpeciality = specialitySDJpaService.findById(1L);
 
+        // then
         assertThat(findSpeciality).isNotNull();
-
         verify(specialtyRepository).findById(anyLong());
+
+        then(specialtyRepository).should().findById(anyLong());
+        then(specialtyRepository).should(times(1)).findById(anyLong());
+        then(specialtyRepository).shouldHaveNoMoreInteractions();
     }
 
     @Test
